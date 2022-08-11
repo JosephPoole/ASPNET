d033f8c44cae28b7060e1ca8c75cccff83705551
Joseph Poole <Joseph.poole90@gmail.com>
2022-08-11 13:14:20 -0500

    Delete added

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -54,5 +54,11 @@ namespace Testing.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
+
     }
 }
